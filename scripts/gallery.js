d753e841255af483840f7e733ed886b3b8c04708
IvanGallardo99 <igallardo@amarillocollege.com>
2026-04-19 13:27:23 -0500

    Add files via upload

---
+++ scripts/gallery.js
@@ -0,0 +1,112 @@
+"use strict";
+/*    Cozy Bed & Breakfast Gallery
+      Uses Chapter 5 slideshow techniques to display a clickable image gallery.
+      Author: Ivan Gallardo
+      Filename: gallery.js
+*/
+
+window.addEventListener("load", createLightbox);
+
+function createLightbox() {
+    // Get the gallery container from the page
+    let lightBox = document.getElementById("lightbox");
+
+    // Create the main parts of the gallery
+    let lbTitle = document.createElement("h1");
+    let lbCounter = document.createElement("div");
+    let lbPrev = document.createElement("div");
+    let lbNext = document.createElement("div");
+    let lbPlay = document.createElement("div");
+    let lbImages = document.createElement("div");
+
+    // Add the gallery title
+    lightBox.appendChild(lbTitle);
+    lbTitle.id = "lbTitle";
+    lbTitle.textContent = lightboxTitle;
+
+    // Add the image counter
+    lightBox.appendChild(lbCounter);
+    lbCounter.id = "lbCounter";
+    let currentImg = 1;
+    lbCounter.textContent = currentImg + " / " + imgCount;
+
+    // Add the previous button
+    lightBox.appendChild(lbPrev);
+    lbPrev.id = "lbPrev";
+    lbPrev.innerHTML = "&#9664;";
+    lbPrev.onclick = showPrev;
+
+    // Add the next button
+    lightBox.appendChild(lbNext);
+    lbNext.id = "lbNext";
+    lbNext.innerHTML = "&#9654;";
+    lbNext.onclick = showNext;
+
+    // Add the play/pause button
+    lightBox.appendChild(lbPlay);
+    lbPlay.id = "lbPlay";
+    lbPlay.innerHTML = "&#9199;";
+    let timeID;
+    lbPlay.onclick = function () {
+        if (timeID) {
+            window.clearInterval(timeID);
+            timeID = undefined;
+        } else {
+            showNext();
+            timeID = window.setInterval(showNext, 1500);
+        }
+    };
+
+    // Add the image container
+    lightBox.appendChild(lbImages);
+    lbImages.id = "lbImages";
+
+    // Add each image to the gallery
+    for (let i = 0; i < imgCount; i++) {
+        let image = document.createElement("img");
+        image.src = imgFiles[i];
+        image.alt = imgCaptions[i];
+        image.onclick = createOverlay;
+        lbImages.appendChild(image);
+    }
+
+    // Show next image
+    function showNext() {
+        lbImages.appendChild(lbImages.firstElementChild);
+        (currentImg < imgCount) ? currentImg++ : currentImg = 1;
+        lbCounter.textContent = currentImg + " / " + imgCount;
+    }
+
+    // Show previous image
+    function showPrev() {
+        lbImages.insertBefore(lbImages.lastElementChild, lbImages.firstElementChild);
+        (currentImg > 1) ? currentImg-- : currentImg = imgCount;
+        lbCounter.textContent = currentImg + " / " + imgCount;
+    }
+
+    // Create fullscreen overlay when image is clicked
+    function createOverlay() {
+        let overlay = document.createElement("div");
+        overlay.id = "lbOverlay";
+
+        let figureBox = document.createElement("figure");
+        overlay.appendChild(figureBox);
+
+        let overlayImage = this.cloneNode(true);
+        figureBox.appendChild(overlayImage);
+
+        let overlayCaption = document.createElement("figcaption");
+        overlayCaption.textContent = this.alt;
+        figureBox.appendChild(overlayCaption);
+
+        let closeBox = document.createElement("div");
+        closeBox.id = "lbOverlayClose";
+        closeBox.innerHTML = "&times;";
+        closeBox.onclick = function () {
+            document.body.removeChild(overlay);
+        };
+        overlay.appendChild(closeBox);
+
+        document.body.appendChild(overlay);
+    }
+}
---
+++ scripts/gallery_data.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/* Cozy Bed & Breakfast Gallery Data
+   Author: Ivan Gallardo
+*/
+
+// Gallery title
+let lightboxTitle = "Cozy Bed & Breakfast Gallery";
+
+// Image file names
+let imgFiles = new Array(5);
+imgFiles[0] = "images/bed_breakfast.png";
+imgFiles[1] = "images/breakfast.png";
+imgFiles[2] = "images/garden_view.png";
+imgFiles[3] = "images/lobby.png";
+imgFiles[4] = "images/view.png";
+
+// Image captions
+let imgCaptions = new Array(5);
+imgCaptions[0] = "Cozy bedroom with mountain view and breakfast in bed";
+imgCaptions[1] = "Fresh homemade breakfast served daily";
+imgCaptions[2] = "Relaxing garden patio surrounded by nature";
+imgCaptions[3] = "Cozy indoor lounge area for relaxing";
+imgCaptions[4] = "Beautiful scenic views for a peaceful getaway";
+
+// Count of images
+let imgCount = imgFiles.length;
